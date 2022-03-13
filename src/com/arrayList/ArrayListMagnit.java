8ec140b72bd068a05e5c923fb89df8dc54d636c0
Alexandr Kuizn <kineva1992@gmail.com>
2022-03-13 12:00:43 +0300

    add ArrayListMagnit

---
+++ src/com/arrayList/ArrayListMagnit.java
@@ -0,0 +1,40 @@
+package com.arrayList;
+import java.util.ArrayList;
+
+public class ArrayListMagnit {
+
+    public static void printAl(ArrayList<String> al)
+    {
+        for(String element : al)
+        {
+            System.out.print(element +" ");
+        }
+        System.out.println(" ");
+
+    }
+
+    public static void main(String[] args)
+    {
+        ArrayList<String> a = new ArrayList<String>();
+
+        a.add(0,"zero");
+        a.add(1,"one");
+        a.add(2,"two");
+        a.add(3,"three");
+        printAl(a);
+        if(a.contains("three"))
+        {
+            a.add("four");
+        }
+
+        a.remove(2);
+        if(a.contains("two"))
+            a.add("2.2");
+        printAl(a);
+        if(a.indexOf("four") != 4)
+            a.add(4,"4.2");
+        printAl(a);
+        printAl(a);
+    }
+
+}
